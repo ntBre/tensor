0987b96884c91954a41d1e4fc81fa315c783f76f
Brent Westbrook <brentrwestbrook@gmail.com>
2022-12-14 08:48:47 -0500

    add tensor5 as vec of tensor4

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,7 @@
 
 pub mod tensor3;
 pub mod tensor4;
+pub mod tensor5;
 pub use tensor3::Tensor3;
 pub use tensor4::Tensor4;
+pub use tensor5::Tensor5;
---
+++ src/tensor5.rs
@@ -0,0 +1,37 @@
+use std::ops::{Index, IndexMut};
+
+use crate::Tensor4;
+
+pub struct Tensor5 {
+    data: Vec<Tensor4>,
+}
+
+impl Tensor5 {
+    pub fn zeros(i: usize, j: usize, k: usize, l: usize, m: usize) -> Self {
+        Self {
+            data: vec![Tensor4::zeros(j, k, l, m); i],
+        }
+    }
+}
+
+impl Index<(usize, usize, usize, usize, usize)> for Tensor5 {
+    type Output = f64;
+
+    fn index(
+        &self,
+        index: (usize, usize, usize, usize, usize),
+    ) -> &Self::Output {
+        let (i, j, k, l, m) = index;
+        &self.data[i][(j, k, l, m)]
+    }
+}
+
+impl IndexMut<(usize, usize, usize, usize, usize)> for Tensor5 {
+    fn index_mut(
+        &mut self,
+        index: (usize, usize, usize, usize, usize),
+    ) -> &mut Self::Output {
+        let (i, j, k, l, m) = index;
+        &mut self.data[i][(j, k, l, m)]
+    }
+}
